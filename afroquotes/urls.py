db8c6288e5c228007173c862ed508e5a19daa633
Muizz Animashaun <anmshmz@gmail.com>
2023-12-30 14:54:27 +0000

    implemented session authentication with authentication()

--- afroquotes/urls.py
+++ afroquotes/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.AllQuotes.as_view(), name="index"),
     # path("quotes", views.get_all_quotes, name="quotes"),
-    path("login", views.UserAuth.as_view(), name="login"),
+    path("login", views.login_view, name="login"),
    
-    path('register', views.CreateUserAPIView.as_view(), name='register'),
+    path('register', views.register, name='register'),
     path("logout", views.logout_view, name="logout"),
     path("submitquote", views.SubmitQuoteClass.as_view(), name="submitQuote"),
     path("search", views.search, name="search"),
--- afroquotes/views.py
+++ afroquotes/views.py
@@ -51,6 +51,59 @@ class searchForm(forms.Form):
 class SuggestImprove(forms.Form):
     comment = forms.CharField(min_length=32, max_length=300)
 
+
+
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "afroquotes/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "afroquotes/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "afroquotes/register.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "afroquotes/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "afroquotes/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
 class CreateUserAPIView(CreateAPIView):
     serializer_class = CreateUserSerializer
     permission_classes = [AllowAny]
